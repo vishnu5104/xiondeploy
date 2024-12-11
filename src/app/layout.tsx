a73e80a4dfc6e8a4b875ad8590f027dd1b62b5ec
vishnu5104 <vishnus510412@gmail.com>
2024-12-11 11:38:45 +0530

    up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,8 +7,7 @@ import '@burnt-labs/abstraxion/dist/index.css';
 import '@burnt-labs/ui/dist/index.css';
 
 const inter = Inter({ subsets: ['latin'] });
-export const seatContractAddress =
-  'xion1z70cvc08qv5764zeg3dykcyymj5z6nu4sqr7x8vl4zjef2gyp69s9mmdka';
+
 export default function RootLayout({
   children,
 }: {
