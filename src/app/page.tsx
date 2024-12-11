f183dbe9fbeb72ca079f8379c10b09a2640853c9
vishnu5104 <vishnus510412@gmail.com>
2024-12-11 11:42:00 +0530

    up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,6 @@ import {
 import { Button } from '@burnt-labs/ui';
 import '@burnt-labs/ui/dist/index.css';
 import type { ExecuteResult } from '@cosmjs/cosmwasm-stargate';
-import { seatContractAddress } from './layout';
 
 type ExecuteResultOrUndefined = ExecuteResult | undefined;
 export default function Page(): JSX.Element {
@@ -52,7 +51,7 @@ export default function Page(): JSX.Element {
     try {
       const claimRes = await client?.execute(
         account.bech32Address,
-        seatContractAddress,
+        'sjdbds',
         msg,
         {
           amount: [{ amount: '1', denom: 'uxion' }],
